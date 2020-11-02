4b2ffa1f56847392d45d06027ce474eb8084e3d7
kayleety <65580840+kayleety@users.noreply.github.com>
2020-11-02 03:07:26 -0500

    fixed the main method

--- hex.py
+++ hex.py
@@ -28,15 +28,18 @@ def xor(mode, key, inp):
             final += hexVal[2:] + " "
 
 #main method
-mode = sys.argv[1]
-keyfile = sys.argv[2]
+if __name__ == "__main__":
+    mode = sys.argv[1]
+    keyfile = sys.argv[2]
 
-inpfile = sys.argv[3]
-key = open(keyfile).read()[:-1] #removes the mandatory \n at the end of the file to support one line messages.
-inp = open(inpfile).read()[:-1] #removes the mandatory \n at the end of the file to support one line messages.
-debug = False
+    inpfile = sys.argv[3]
+    key = open(keyfile).read()[:-1] #removes the mandatory \n at the end of the file to support one line messages.
+    inp = open(inpfile).read()[:-1] #removes the mandatory \n at the end of the file to support one line messages.
+    debug = False
 
-if(debug):
-    print("mode:"+mode)
-    print("key: "+key)
-    print("inp: "+inp)
+    if(debug):
+        print("mode:"+mode)
+        print("key: "+key)
+        print("inp: "+inp)
+
+    
